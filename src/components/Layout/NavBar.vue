f42c6b8b7afe5f5067d61fd3145ece011259ebb9
petrero <petrero@gmail.com>
2022-04-22 19:36:36 +0300

    14.80(2)
    Hard code the classes needed to activate and show the burger menu

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -8,14 +8,20 @@
 					Noteballs
 				</a>
 
-				<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+				<a
+					role="button"
+					class="navbar-burger is-active"
+					aria-label="menu"
+					aria-expanded="false"
+					data-target="navbarBasicExample"
+				>
 					<span aria-hidden="true"></span>
 					<span aria-hidden="true"></span>
 					<span aria-hidden="true"></span>
 				</a>
 			</div>
 
-			<div id="navbarBasicExample" class="navbar-menu">
+			<div id="navbarBasicExample" class="navbar-menu is-active">
 			<div class="navbar-start">
 			</div>
 
