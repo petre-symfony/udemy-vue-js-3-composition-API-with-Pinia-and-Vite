2253c60b852da62a4a47e2bbc7e0a4908cdadac6
petrero <petrero@gmail.com>
2022-04-22 17:40:33 +0300

    13.73(1)
    Add a route to our app.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
+import VueNotes from "@/views/VueNotes.vue"
 
 const router = createRouter({
 	history: createWebHashHistory(),
-	routes: []
+	routes: [
+		{
+			path: '/',
+			name: 'notes',
+			component: VueNotes
+		}
+	]
 })
 
 createApp(App).use(router).mount('#app')
---
+++ src/views/VueNotes.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="notes">
+		<h1>Notes</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "VueNotes"
+}
+</script>
+
+<style scoped>
+
+</style>
