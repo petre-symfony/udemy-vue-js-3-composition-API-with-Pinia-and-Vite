629a47b75d2fe7114c8de3b323a452ec486195db
petrero <petrero@gmail.com>
2022-04-23 12:21:06 +0300

    16.92(2)
    Create note store

---
+++ src/stores/storeNotes.js
@@ -0,0 +1,19 @@
+// stores/counter.js
+import { defineStore } from 'pinia'
+
+export const useStoreNotes = defineStore('storeNotes', {
+	state: () => {
+		return {
+			notes: [
+				{
+					id: 'id1',
+					content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.'
+				},
+				{
+					id: 'id2',
+					content: 'This is a shorter note'
+				}
+			]
+		}
+	}
+})
